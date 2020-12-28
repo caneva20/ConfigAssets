6a0be016d75fa6b6e2da2a344f260c67b8944031
Fabricio Caneva <hardlymersk@gmail.com>
2020-12-28 16:54:21 -0300

    WIP: update project structure

---
+++ Config.cs
@@ -0,0 +1,20 @@
+using caneva20.ConfigAssets.Loading;
+using UnityEngine;
+
+namespace caneva20.ConfigAssets {
+    public class Config : ScriptableObject { }
+
+    public class Config<T> : Config where T : Config {
+        private static T _instance;
+
+        public static T Instance {
+            get {
+                if (_instance == null) {
+                    _instance = ConfigLoader.Load<T>();
+                }
+
+                return _instance;
+            }
+        }
+    }
+}

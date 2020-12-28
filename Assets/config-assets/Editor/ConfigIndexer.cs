4d128a3b6a5ba1a53cb251b96f181c9fce3bb50c
Fabricio Caneva <hardlymersk@gmail.com>
2020-12-28 19:12:52 -0300

    create .asset on script load

--- Assets/config-assets/Editor/ConfigIndexer.cs
+++ Assets/config-assets/Editor/ConfigIndexer.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using me.caneva20.ConfigAssets.Loading;
 using UnityEditor;
 using UnityEditor.Callbacks;
 
@@ -7,6 +9,17 @@ namespace me.caneva20.ConfigAssets.Editor {
     public static class ConfigIndexer {
         [DidReloadScripts]
         private static void OnScriptReload() {
+            var configType = typeof(Config);
+            
+            var types = AppDomain.CurrentDomain.GetAssemblies()
+               .SelectMany(x => x.GetTypes())
+               .Where(x =>
+                    !x.IsInterface && !x.IsAbstract && x != configType && configType.IsAssignableFrom(x)); 
+            
+            foreach (var type in types) {
+                ConfigLoader.Load(type);
+            }
+
             var guids = AssetDatabase.FindAssets($"t:{typeof(Config)}");
 
             var configs = guids.Select(guid => AssetDatabase.LoadAssetAtPath<Config>(AssetDatabase.GUIDToAssetPath(guid)));

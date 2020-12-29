513118f7c92e773ff2a31e53aa49e75ff879e152
Fabricio Caneva <hardlymersk@gmail.com>
2020-12-28 21:09:14 -0300

    automatically generates SettingsProvider for each config class

--- Editor/DefaultsEditor.cs
+++ Editor/DefaultsEditor.cs
@@ -7,13 +7,15 @@ namespace me.caneva20.ConfigAssets.Editor {
     [CustomEditor(typeof(Defaults))]
     public class DefaultsEditor : UnityEditor.Editor {
         private readonly Regex _resourcesFolder = new Regex("[Rr]esources");
-        
+
         private SerializedProperty _baseDirectory;
+        private SerializedProperty _codeGenDirectory;
         private SerializedProperty _appendNamespaceToFile;
         private SerializedProperty _nameSpaceLength;
 
         private void OnEnable() {
             _baseDirectory = serializedObject.FindProperty("_baseDirectory");
+            _codeGenDirectory = serializedObject.FindProperty("_codeGenDirectory");
             _appendNamespaceToFile = serializedObject.FindProperty("_appendNamespaceToFile");
             _nameSpaceLength = serializedObject.FindProperty("_nameSpaceLength");
         }
@@ -21,42 +23,52 @@ namespace me.caneva20.ConfigAssets.Editor {
         public override void OnInspectorGUI() {
             #region Base directory
 
-            using (var scope = new EditorGUILayout.HorizontalScope()) {
-                EditorGUILayout.LabelField($"{_baseDirectory.displayName}\t Assets\\");
-                _baseDirectory.stringValue = EditorGUILayout.TextField(_baseDirectory.stringValue);
-
-                if (GUILayout.Button("...", EditorStyles.miniButton)) {
-                    var path = EditorUtility.SaveFolderPanel(_baseDirectory.displayName,
-                        Path.Combine(Application.dataPath, _baseDirectory.stringValue),
-                        "Resources");
-
-                    path = path.Replace($"{Application.dataPath}/", "");
-                    path = path.Replace("/", "\\");
-
-                    _baseDirectory.stringValue = path;
-                }
-            }
-            
-            EditorGUILayout.HelpBox("The base directory used to store the configs", MessageType.Info);
+            DirectorySelector(_baseDirectory, "Resources", "The base directory used to store the configs");
 
             if (!_resourcesFolder.IsMatch(_baseDirectory.stringValue)) {
-                EditorGUILayout.HelpBox("Base directory MUST contain a 'Resources' folder or be inside one", MessageType.Error);
+                EditorGUILayout.HelpBox(
+                    "Base directory MUST contain a 'Resources' folder or be inside one",
+                    MessageType.Error);
             }
 
             #endregion
-            
-            Prop(_appendNamespaceToFile, "Should the namespace of the class be appended to the file name of the asset?\n\n" +
+
+            DirectorySelector(_codeGenDirectory, "Resources",
+                "The directory used to save auto-generated files");
+
+            Prop(_appendNamespaceToFile,
+                "Should the namespace of the class be appended to the file name of the asset?\n\n" +
                 "NOTE: This setting is ignored for classes with no namespace");
-            
+
             if (_appendNamespaceToFile.boolValue) {
                 EditorGUI.indentLevel++;
-                Prop(_nameSpaceLength, "How many segments of the namespace should be used?\n\n" +
+                Prop(_nameSpaceLength,
+                    "How many segments of the namespace should be used?\n\n" +
                     "0: None\n-1: The full namespace\n\nNOTE: Segments are the sum of dots(.) in the namespace plus 1, i.e caneva20.ConfigAssets = 2");
-                
+
                 EditorGUI.indentLevel--;
             }
 
             serializedObject.ApplyModifiedProperties();
+        }
+
+        private static void DirectorySelector(SerializedProperty property, string defaultDirectory, string info) {
+            EditorGUILayout.HelpBox(info, MessageType.Info);
+            
+            using (new EditorGUILayout.HorizontalScope()) {
+                EditorGUILayout.LabelField($"{property.displayName}\t Assets\\");
+                property.stringValue = EditorGUILayout.TextField(property.stringValue);
+
+                if (GUILayout.Button("...", EditorStyles.miniButton)) {
+                    var path = EditorUtility.SaveFolderPanel(property.displayName,
+                        Path.Combine(Application.dataPath, property.stringValue), defaultDirectory);
+
+                    path = path.Replace($"{Application.dataPath}/", "");
+                    path = path.Replace("/", "\\");
+
+                    property.stringValue = path;
+                }
+            }
         }
 
         private static void Prop(SerializedProperty property, string info) {

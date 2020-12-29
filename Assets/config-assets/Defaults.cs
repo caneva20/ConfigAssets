d56342ce7ab393eca5dbab0e120030dee16642a6
Fabricio Caneva <hardlymersk@gmail.com>
2020-12-28 21:09:14 -0300

    automatically generates SettingsProvider for each config class

--- Assets/config-assets/Defaults.cs
+++ Assets/config-assets/Defaults.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 namespace me.caneva20.ConfigAssets {
     public class Defaults : Config<Defaults> {
         [SerializeField] private string _baseDirectory = "Configurations\\Resources";
+        [SerializeField] private string _codeGenDirectory = "Configurations";
         [SerializeField] private bool _appendNamespaceToFile = true;
         [SerializeField] private int _nameSpaceLength = 2;
 
         public bool AppendNamespaceToFile => _appendNamespaceToFile;
         public string BaseDirectory => _baseDirectory;
+        public string CodeGenDirectory => _codeGenDirectory;
         public int NameSpaceLength => _nameSpaceLength;
     }
 }

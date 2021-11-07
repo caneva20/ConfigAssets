02c87a743fe9ee4bd57652659577f5e55dff66fe
Fabricio Calheiros <hardlymersk@gmail.com>
2021-11-06 23:28:09 -0300

    replace code gen with T4 templates

---
+++ Editor/SettingsProviderGeneratorPartial.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace me.caneva20.ConfigAssets.Editor {
+    partial class SettingsProviderGenerator {
+        public IEnumerable<ProviderDefinition> Providers { get; }
+
+        public SettingsProviderGenerator(IEnumerable<ProviderDefinition> providers) {
+            Providers = providers;
+        }
+    }
+}

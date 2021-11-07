d644bf4306cfe57278e132ab900af2cb20310f0c
Fabricio Calheiros <hardlymersk@gmail.com>
2021-11-07 20:09:13 -0300

    move SettingsProvider generation steps to build class

--- Editor/ConfigIndexer.cs
+++ Editor/ConfigIndexer.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using me.caneva20.ConfigAssets.Loading;
 using UnityEditor;
@@ -18,9 +17,9 @@ namespace me.caneva20.ConfigAssets.Editor {
             }
 
             UpdatePreloadList();
-
-            var providers = GetProviders(types);
-            GenerateCodeFile(providers);
+            SettingsProviderBuilder.Build(types);
+            
+            RefreshAssetDatabase();
         }
 
         private static IEnumerable<Type> FindConfigurations() {
@@ -46,47 +45,7 @@ namespace me.caneva20.ConfigAssets.Editor {
             PlayerSettings.SetPreloadedAssets(preload.ToArray());
         }
 
-        private static IEnumerable<ProviderDefinition> GetProviders(IEnumerable<Type> providerTypes) {
-            foreach (var configType in providerTypes) {
-                var attribute = ConfigAttribute.Find(configType);
-
-                if (attribute?.EnableProvider == false) {
-                    continue;
-                }
-
-                var name = configType?.FullName;
-                var displayName = attribute?.DisplayName ?? configType?.Name;
-
-                var keywords = (attribute?.Keywords ?? Array.Empty<string>()).Select(x => $"\"{x}\"").ToList();
-
-                yield return new ProviderDefinition {
-                    Name = name?.Replace(".", ""),
-                    NamespacedName = configType?.FullName,
-                    DisplayName = displayName,
-                    Scope = (attribute?.Scope ?? SettingsScope.Project).ToString(),
-                    Keywords = keywords.Count == 0 ? "null" : $"new string[] {{{string.Join(", ", keywords)}}}",
-                };
-            }
-        }
-
-        private static void GenerateCodeFile(IEnumerable<ProviderDefinition> providers) {
-            var generator = new SettingsProviderGenerator {
-                Session = new Dictionary<string, object> {
-                    { "_providers", providers.ToArray() }
-                }
-            };
-
-            generator.Initialize();
-            var csharpCode = generator.TransformText();
-            
-            var dir = $"Assets/{Defaults.Instance.CodeGenDirectory}";
-
-            if (!Directory.Exists(dir)) {
-                Directory.CreateDirectory(dir);
-            }
-            
-            File.WriteAllText($@"{dir}/ConfigAssetsSettingsProvider.g.cs", csharpCode);
-
+        private static void RefreshAssetDatabase() {
             try {
                 AssetDatabase.Refresh();
             } catch (Exception e) {

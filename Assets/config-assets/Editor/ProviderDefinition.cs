e7733271f847e2308c91326573c4b3d1437bb634
Fabricio Calheiros <hardlymersk@gmail.com>
2021-11-06 23:28:09 -0300

    replace code gen with T4 templates

---
+++ Assets/config-assets/Editor/ProviderDefinition.cs
@@ -0,0 +1,9 @@
+﻿namespace me.caneva20.ConfigAssets.Editor {
+    public class ProviderDefinition {
+        public string Name { get; set; }
+        public string NamespacedName { get; set; }
+        public string DisplayName { get; set; }
+        public string[] Keywords { get; set; }
+        public SettingsScope Scope { get; set; }
+    }
+}

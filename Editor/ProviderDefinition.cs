1c8cee804e8c5585c2ae08a9f9141b6f95bbe9c7
Fabricio Calheiros <hardlymersk@gmail.com>
2023-06-18 19:30:57 -0300

    remove unused code

--- Editor/ProviderDefinition.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace me.caneva20.ConfigAssets.Editor {
-    public class ProviderDefinition {
-        public string Name { get; set; }
-        public string NamespacedName { get; set; }
-        public string DisplayName { get; set; }
-        public string Keywords { get; set; }
-        public string Scope { get; set; }
-    }
-}

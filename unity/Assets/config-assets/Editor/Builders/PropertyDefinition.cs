591fef5db26eaa54184be267d18b6cdde02018cd
Fabricio Calheiros <hardlymersk@gmail.com>
2023-06-18 19:25:21 -0300

    replace EnhancedConfigurationBuilder with source generator

--- unity/Assets/config-assets/Editor/Builders/PropertyDefinition.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace me.caneva20.ConfigAssets.Editor.Builders {
-    public class PropertyDefinition {
-        public string Name { get; set; }
-        public string TargetName { get; set; }
-        public string Type { get; set; }
-    }
-}

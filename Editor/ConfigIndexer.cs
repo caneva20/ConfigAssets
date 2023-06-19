7e0820f041a8247bd3113e19f4ad242cc16c46a2
Fabricio Calheiros <hardlymersk@gmail.com>
2023-06-19 19:58:49 -0300

    rename 'Loading' namespace to 'Infrastructure'

--- Editor/ConfigIndexer.cs
+++ Editor/ConfigIndexer.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using ConfigAssets.Loading;
+using ConfigAssets.Infrastructure;
 using ConfigAssets.Logging;
 
 namespace ConfigAssets.Editor {

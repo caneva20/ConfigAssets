df21dcbe3bc7a89c0de8bfe5c3b9e3d154643743
Fabricio Calheiros <hardlymersk@gmail.com>
2023-06-18 23:17:01 -0300

    use packages folder to store generated files

---
+++ unity/Assets/config-assets/Editor/PackageCreator.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using ConfigAssets.Logging;
+using UnityEngine;
+
+namespace ConfigAssets.Editor {
+    public static class PackageCreator {
+        public static string Create(string name, string version, string displayName, string description) {
+#if !UNITY_EDITOR
+            return null;
+#endif
+
+            var absolutePath = Path.Combine(Application.dataPath.Replace("Assets", ""), "Packages", name);
+            var relativePath = Path.Combine("Packages", name);
+
+            Directory.CreateDirectory(absolutePath);
+
+            var filePath = Path.Combine(absolutePath, "package.json");
+
+            if (File.Exists(filePath)) {
+                return relativePath;
+            }
+
+            var definition = $@"{{
+    ""name"": ""{name}"",
+    ""version"": ""{version}"",
+    ""displayName"": ""{displayName}"",
+    ""description"": ""{description}""
+}}";
+
+            File.WriteAllText(filePath, definition);
+
+#if UNITY_EDITOR
+            UnityEditor.AssetDatabase.Refresh();
+            UnityEditor.Compilation.CompilationPipeline.RequestScriptCompilation();
+            UnityEditor.AssetDatabase.Refresh();
+#endif
+
+            ConfigAssetLogger.LogInformation($"Package {name} created");
+
+            return relativePath;
+        }
+    }
+}

c21fd9e6c5a0699c81e45bb2bbdc679ec7c08973
Fabricio Calheiros <hardlymersk@gmail.com>
2023-06-20 20:44:14 -0300

    remove asset creation from ConfigLoader.cs

--- ConfigAttribute.cs
+++ ConfigAttribute.cs
@@ -3,16 +3,11 @@ using System;
 namespace ConfigAssets {
     [AttributeUsage(AttributeTargets.Class)]
     public class ConfigAttribute : Attribute {
-        public string FileName { get; set; }
         public string DisplayName { get; set; }
         public bool EnableProvider { get; set; } = true;
         public SettingsScope Scope { get; set; } = SettingsScope.Project;
         public string[] Keywords { get; set; } = { };
 
         public bool GenerateSingleton { get; set; }
-
-        public static ConfigAttribute Find(Type type) {
-            return (ConfigAttribute) GetCustomAttribute(type, typeof(ConfigAttribute));
-        }
     }
 }

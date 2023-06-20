d7ecb869641d00f22e7f4fb38d229cedc670679c
Fabricio Calheiros <hardlymersk@gmail.com>
2023-06-19 23:51:56 -0300

    add asset preloading utility

---
+++ unity/Assets/config-assets/Infrastructure/EditorPreloadedAssetService.cs
@@ -0,0 +1,21 @@
+﻿#if UNITY_EDITOR
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace ConfigAssets.Infrastructure {
+    public class EditorPreloadedAssetService : IPreloadedAssetService {
+        public void Add(Object asset) {
+            var assets = PlayerSettings.GetPreloadedAssets().Where(x => x != null).ToList();
+
+            if (assets.Contains(asset)) {
+                return;
+            }
+
+            assets.Add(asset);
+
+            PlayerSettings.SetPreloadedAssets(assets.ToArray());
+        }
+    }
+}
+#endif

ca16e84d0ad79921aeaa91422e335b0eb9639aab
Fabricio Calheiros <hardlymersk@gmail.com>
2023-06-20 20:39:36 -0300

    update metadata generation

---
+++ Metadata/AssetMetadata.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConfigAssets.Metadata {
+    public class AssetMetadata {
+        public Type Type { get; }
+        public string AssetName { get; }
+
+        public AssetMetadata(Type type) {
+            Type = type;
+            AssetName = $"{Type.FullName}.asset";
+        }
+    }
+}

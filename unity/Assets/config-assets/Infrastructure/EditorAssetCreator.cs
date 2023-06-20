8a076e6742a32166286f09ad7768f8cf6fee7577
Fabricio Calheiros <hardlymersk@gmail.com>
2023-06-19 22:58:07 -0300

    add EditorAssetCreator

---
+++ unity/Assets/config-assets/Infrastructure/EditorAssetCreator.cs
@@ -0,0 +1,40 @@
+﻿#if UNITY_EDITOR
+using System;
+using ConfigAssets.Package;
+using ConfigAssets.Package.Models;
+using UnityEditor;
+using UnityEngine;
+
+namespace ConfigAssets.Infrastructure {
+    public class EditorAssetCreator : IAssetCreator {
+        private readonly IPackageProvider _packageProvider;
+
+        public EditorAssetCreator(IPackageProvider packageProvider) {
+            _packageProvider = packageProvider;
+        }
+
+        public object CreateAsset<T>() where T : ScriptableObject {
+            var type = typeof(T);
+            var assetName = MakeAssetName(type);
+            var assetPath = _packageProvider.GetResourceLocation(PackageType.GeneratedAssets, assetName);
+
+            var asset = ScriptableObject.CreateInstance(type);
+
+            AssetDatabase.CreateAsset(asset, assetPath);
+
+            RefreshAssets();
+
+            return asset;
+        }
+
+        private static string MakeAssetName(Type type) {
+            return $"{type.FullName}.asset";
+        }
+
+        private static void RefreshAssets() {
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+        }
+    }
+}
+#endif

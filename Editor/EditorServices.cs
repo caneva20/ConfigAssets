6931b030f14395a37c60a83bd26ceb9421decac1
Fabricio Calheiros <hardlymersk@gmail.com>
2023-06-20 23:04:13 -0300

    rename ProcessingPipeline to EditorPipelineExecutor move EditorPipelineExecutor to editor project

--- Editor/EditorServices.cs
+++ Editor/EditorServices.cs
@@ -1,4 +1,8 @@
-﻿namespace ConfigAssets.Editor {
+﻿using ConfigAssets.Editor.Pipelines;
+using ConfigAssets.Pipelines;
+
+namespace ConfigAssets.Editor {
     public static class EditorServices {
+        public static readonly IPipelineExecutor PipelineExecutor = new EditorPipelineExecutor();
     }
 }

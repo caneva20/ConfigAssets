871813241b332b67e0b0aab1bd2ae7efa50d1fee
Fabricio Calheiros <hardlymersk@gmail.com>
2023-06-20 23:04:13 -0300

    rename ProcessingPipeline to EditorPipelineExecutor move EditorPipelineExecutor to editor project

--- unity/Assets/config-assets/Editor/Pipelines/EditorPipelineExecutor.cs
+++ unity/Assets/config-assets/Editor/Pipelines/EditorPipelineExecutor.cs
@@ -1,14 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using ConfigAssets.Pipelines;
 using Unity.EditorCoroutines.Editor;
 
-namespace ConfigAssets.Pipelines {
-    public static class ProcessingPipeline {
-        public static void Process(float delay, IEnumerable<IPipelineStep> steps) {
+namespace ConfigAssets.Editor.Pipelines {
+    public class EditorPipelineExecutor : IPipelineExecutor {
+        public void Process(float delay, IEnumerable<IPipelineStep> steps) {
             EditorCoroutineUtility.StartCoroutineOwnerless(ProcessEnumerator(delay, steps));
         }
 
-        public static IEnumerator ProcessEnumerator(float delay, IEnumerable<IPipelineStep> steps) {
+        public IEnumerator ProcessEnumerator(float delay, IEnumerable<IPipelineStep> steps) {
             foreach (var step in steps) {
                 if (step.Run()) {
                     yield return new EditorWaitForSeconds(delay);

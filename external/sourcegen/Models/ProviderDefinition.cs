8b4cdc74373b7fffb154d8f7a27c5cc7b9469026
Fabricio Calheiros <hardlymersk@gmail.com>
2023-06-20 21:40:23 -0300

    remove ProviderDefinition

--- external/sourcegen/Models/ProviderDefinition.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ConfigAssets.Sourcegen.Models {
-    public class ProviderDefinition {
-        public string Name { get; set; }
-        public string FullyQualifiedName { get; set; }
-        public string DisplayName { get; set; }
-        public string Keywords { get; set; }
-        public int Scope { get; set; }
-    }
-}
---
+++ external/sourcegen/Utils/StringExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace ConfigAssets.Sourcegen.Utils {
+    public static class StringExtensions {
+        public static string Sanitize(this string str) {
+            return str.Replace(".", "");
+        }
+    }
+}

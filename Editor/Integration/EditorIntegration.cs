10956e4b4e08dd53f53503bb8786aa2a6c3d38e4
Fabricio Calheiros <hardlymersk@gmail.com>
2023-06-20 23:52:32 -0300

    add editor integration to automatically create required assets

---
+++ Editor/Integration/EditorIntegration.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ConfigAssets.Metadata;
+using ConfigAssets.Package.Models;
+using ConfigAssets.Pipelines;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+namespace ConfigAssets.Editor.Integration {
+    public static class EditorIntegration {
+        [DidReloadScripts(-1)]
+        public static void OnScriptReload() {
+            EditorServices.PipelineExecutor.Process(0,
+                new[] {
+                    new PipelineStep(() => EditorServices.PackageProvider.CreatePackage(PackageType.GeneratedAssets)),
+                    new PipelineStep(CreateAssets)
+                });
+        }
+
+        private static void CreateAssets() {
+            foreach (var asset in GetAssets()) {
+                EditorServices.AssetCreator.CreateAsset(asset);
+            }
+        }
+
+        private static IEnumerable<AssetMetadata> GetAssets() {
+            return TypeCache.GetTypesWithAttribute(typeof(ConfigAttribute)).Select(CreateMetadata).Where(NeedsCreation);
+        }
+
+        private static AssetMetadata CreateMetadata(Type field) {
+            return new AssetMetadata(field);
+        }
+
+        private static bool NeedsCreation(AssetMetadata asset) {
+            return Services.ConfigLoader.Load<ScriptableObject>(asset) == null;
+        }
+    }
+}

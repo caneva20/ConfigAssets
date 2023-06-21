0b67d25f560a0bbab413ed0f631a132382eab28e
Fabricio Calheiros <hardlymersk@gmail.com>
2023-06-21 19:56:09 -0300

    update release script

--- release.ps1
+++ release.ps1
@@ -1,2 +1,3 @@
-dotnet build -c Release
+dotnet build -c Release .\external\external.sln
+git commit --only .\unity\Assets\config-assets\Plugins\config-assets.sourcegen.dll -m "update sourcegen dll"
 git subtree split --prefix=unity/Assets/config-assets --branch upm

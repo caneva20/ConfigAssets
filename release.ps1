010f8507ab180345e99ca4c3ccb68acfd7dbffa1
Fabricio Calheiros <hardlymersk@gmail.com>
2023-06-21 20:28:31 -0300

    update release.ps1

--- release.ps1
+++ release.ps1
@@ -1,3 +1,6 @@
 dotnet build -c Release .\external\external.sln
 git commit --only .\unity\Assets\config-assets\Plugins\config-assets.sourcegen.dll -m "update sourcegen dll"
 git subtree split --prefix=unity/Assets/config-assets --branch upm
+
+$version = (Get-Content -Raw -Path .\unity\Assets\config-assets\package.json | ConvertFrom-Json).version
+git tag -a $version -m "Version $version" upm

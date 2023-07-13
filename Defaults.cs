d1681c0128c33c4801983d6e16713c0ea99f81bf
Fabricio Calheiros <hardlymersk@gmail.com>
2023-07-13 20:03:13 -0300

    remove logging and default config

--- Defaults.cs
+++
@@ -1,9 +0,0 @@
-using ConfigAssets.Logging;
-using UnityEngine;
-
-namespace ConfigAssets {
-    [Config(DisplayName = "ConfigAssets settings", GenerateSingleton = true)]
-    public partial class Defaults {
-        [SerializeField] private LoggingLevel _loggingLevel = LoggingLevel.Warning;
-    }
-}
